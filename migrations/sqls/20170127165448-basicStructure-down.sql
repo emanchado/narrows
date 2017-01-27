1c8e09e415e40881a7316a5115a62172bd614268
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2017-01-27 20:38:13 +0100

    Initial database structure for MySQL

---
+++ migrations/sqls/20170127165448-basicStructure-down.sql
@@ -0,0 +1,7 @@
+DROP TABLE narrations;
+DROP TABLE chapters;
+DROP TABLE characters;
+DROP TABLE reactions;
+DROP TABLE users;
+DROP TABLE messages;
+DROP TABLE message_deliveries;
---
+++ migrations/sqls/20170127165448-basicStructure-up.sql
@@ -0,0 +1,60 @@
+CREATE TABLE users (id integer AUTO_INCREMENT PRIMARY KEY,
+                    username varchar(64) UNIQUE,
+                    password varchar(128),
+                    role varchar(64),
+                    email text);
+INSERT INTO users (username, password, role, email)
+  VALUES ('narrator',
+          '$2a$04$NrMPbG7wG26EwqJOun.SLOELYGOmbFs5aECGxhl8suPfVY049NZdG',
+          'admin',
+          'admin@example.com');
+CREATE TABLE narrations (id integer AUTO_INCREMENT PRIMARY KEY,
+                         narrator_id integer,
+                         title varchar(256),
+                         default_audio varchar(256),
+                         default_background_image varchar(256),
+                         FOREIGN KEY (narrator_id) REFERENCES users(id)
+                         ON DELETE RESTRICT);
+CREATE TABLE chapters (id integer AUTO_INCREMENT PRIMARY KEY,
+                       narration_id integer,
+                       title varchar(256),
+                       audio varchar(256),
+                       background_image varchar(256),
+                       main_text text,
+                       created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                       updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                       published timestamp NULL,
+                       FOREIGN KEY (narration_id) REFERENCES narrations(id)
+                       ON DELETE RESTRICT);
+CREATE TABLE characters (id integer AUTO_INCREMENT PRIMARY KEY,
+                         narration_id integer,
+                         player_id integer,
+                         name varchar(256),
+                         token varchar(128),
+                         avatar varchar(256),
+                         description text,
+                         backstory text,
+                         notes text,
+                         FOREIGN KEY (narration_id) REFERENCES narrations(id)
+                         ON DELETE RESTRICT,
+                         FOREIGN KEY (player_id) REFERENCES users(id)
+                         ON DELETE RESTRICT);
+CREATE TABLE reactions (id integer AUTO_INCREMENT PRIMARY KEY,
+                        chapter_id integer references chapters(id) ON DELETE CASCADE,
+                        character_id integer references characters(id) ON DELETE CASCADE,
+                        main_text text);
+CREATE TABLE messages (id integer AUTO_INCREMENT PRIMARY KEY,
+                       chapter_id integer,
+                       sender_id integer,
+                       body text,
+                       sent timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                       FOREIGN KEY (chapter_id) REFERENCES chapters(id)
+                       ON DELETE RESTRICT,
+                       FOREIGN KEY (sender_id) REFERENCES characters(id)
+                       ON DELETE RESTRICT);
+CREATE TABLE message_deliveries (message_id integer,
+                                 recipient_id integer,
+                                 FOREIGN KEY (message_id) REFERENCES messages(id)
+                                 ON DELETE CASCADE,
+                                 FOREIGN KEY (recipient_id) REFERENCES characters(id)
+                                 ON DELETE CASCADE);
