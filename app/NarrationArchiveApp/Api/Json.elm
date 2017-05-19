73cd2c7a221f32a44f9f0c50aadbcbbe690778b1
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2017-05-19 18:38:44 +0200

    Clean up unused imports

--- app/NarrationArchiveApp/Api/Json.elm
+++ app/NarrationArchiveApp/Api/Json.elm
@@ -2,7 +2,6 @@ module NarrationArchiveApp.Api.Json exposing (..)
 
 import Json.Decode as Json exposing (..)
 import Common.Api.Json exposing (parseReaction, parseNarrationOverview)
-import Common.Models exposing (NarrationOverview, ChapterOverview)
 import NarrationArchiveApp.Models exposing (NarrationArchive)
 
 
--- app/NarratorDashboardApp/Api/Json.elm
+++ app/NarratorDashboardApp/Api/Json.elm
@@ -2,7 +2,6 @@ module NarratorDashboardApp.Api.Json exposing (..)
 
 import Json.Decode as Json exposing (..)
 import Common.Api.Json exposing (parseReaction, parseNarrationOverview)
-import Common.Models exposing (NarrationOverview, ChapterOverview)
 import NarratorDashboardApp.Models exposing (NarratorOverview)
 
 
