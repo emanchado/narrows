78269c0273b47866ebe93a45409492efb035d143
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2017-05-19 18:00:21 +0200

    Add a ribbon to non-active narrations

--- app/Common/Views.elm
+++ app/Common/Views.elm
@@ -5,185 +5,170 @@ import Json.Decode
 import Html exposing (Html, h2, div, nav, textarea, button, span, ul, li, img, a, em, strong, text)
 import Html.Attributes exposing (class, rows, value, href, src)
 import Html.Events exposing (defaultOptions, onWithOptions, onClick, onInput)
-import Common.Models exposing (MessageThread, Message, Banner, ReplyInformation, Breadcrumb, ChapterOverview, Narration, NarrationOverview, NarrationStatus(..))
+import Common.Models exposing (MessageThread, Message, Banner, ReplyInformation, Breadcrumb, ChapterOverview, Narration, NarrationOverview, NarrationStatus(..), narrationStatusString)
 
 
 onPreventDefaultClick : msg -> Html.Attribute msg
 onPreventDefaultClick message =
-    onWithOptions
-        "click"
-        { defaultOptions | preventDefault = True }
-        (Json.Decode.succeed message)
+  onWithOptions
+    "click"
+    { defaultOptions | preventDefault = True }
+    (Json.Decode.succeed message)
 
 
 onStopPropagationClick : msg -> Html.Attribute msg
 onStopPropagationClick message =
-    onWithOptions
-        "click"
-        { defaultOptions | stopPropagation = True }
-        (Json.Decode.succeed message)
+  onWithOptions
+    "click"
+    { defaultOptions | stopPropagation = True }
+    (Json.Decode.succeed message)
 
 
 messageView : Message -> Html msg
 messageView message =
-    div [ class "message" ]
-        [ strong []
-            [ text
-                (case message.sender of
-                    Just sender ->
-                        sender.name
-
-                    Nothing ->
-                        "Narrator"
-                )
-            ]
-        , text ": "
-        , span
-            [ class
-                (case message.sender of
-                    Just sender ->
-                        ""
-
-                    Nothing ->
-                        "narrator"
-                )
-            ]
-            [ text message.body ]
+  div [ class "message" ]
+    [ strong []
+        [ text (case message.sender of
+                  Just sender -> sender.name
+                  Nothing -> "Narrator")
         ]
+    , text ": "
+    , span [ class (case message.sender of
+                      Just sender -> ""
+                      Nothing -> "narrator")
+           ]
+        [ text message.body ]
+    ]
 
 
 threadView : Maybe Int -> msg -> (String -> msg) -> msg -> msg -> Maybe ReplyInformation -> MessageThread -> Html msg
 threadView maybeCharacterId showReplyMessage updateReplyMessage sendReplyMessage closeReplyMessage maybeReply thread =
-    let
-        participants =
-            List.map
-                (\c -> c.name)
-                (case maybeCharacterId of
-                    Just characterId ->
-                        (List.filter
-                            (\c -> c.id /= characterId)
-                            thread.participants
-                        )
-
-                    Nothing ->
-                        thread.participants
-                )
-
-        participantString =
-            String.join ", " participants
-
-        participantStringEnd =
-            case maybeCharacterId of
-                Nothing ->
-                    if List.length participants > 1 then
-                        ", and you"
-                    else
-                        " and you"
-
-                Just _ ->
-                    if List.length participants > 0 then
-                        ", the narrator, and you"
-                    else
-                        "the narrator and you"
-
-        participantsDiv =
-            div [ class "thread-participants" ]
-                [ text ("Between " ++ participantString ++ participantStringEnd) ]
-
-        replyButtonDiv =
-            div [ class "btn-bar" ]
-                [ button
-                    [ class "btn btn-small"
-                    , onClick showReplyMessage
-                    ]
-                    [ text "Reply" ]
-                ]
-
-        replyBoxDiv =
-            case maybeReply of
-                Just reply ->
-                    if reply.recipients == thread.participants then
-                        div []
-                            [ textarea
-                                [ rows 4
-                                , value reply.body
-                                , onInput updateReplyMessage
-                                ]
-                                [ text reply.body ]
-                            , div [ class "btn-bar" ]
-                                [ button
-                                    [ class "btn btn-default btn-small"
-                                    , onClick sendReplyMessage
-                                    ]
-                                    [ text "Send" ]
-                                , button
-                                    [ class "btn btn-small"
-                                    , onClick closeReplyMessage
-                                    ]
-                                    [ text "Close" ]
-                                ]
-                            ]
-                    else
-                        replyButtonDiv
-
-                Nothing ->
-                    replyButtonDiv
-    in
-        li []
-            (List.concat
-                [ [ participantsDiv ]
-                , List.map messageView thread.messages
-                , [ replyBoxDiv ]
-                ]
+  let
+    participants =
+      List.map
+        (\c -> c.name)
+        (case maybeCharacterId of
+          Just characterId ->
+            (List.filter
+              (\c -> c.id /= characterId)
+              thread.participants
             )
+
+          Nothing ->
+            thread.participants
+        )
+
+    participantString =
+      String.join ", " participants
+
+    participantStringEnd =
+      case maybeCharacterId of
+        Nothing ->
+          if List.length participants > 1 then
+            ", and you"
+          else
+            " and you"
+
+        Just _ ->
+          if List.length participants > 0 then
+            ", the narrator, and you"
+          else
+            "the narrator and you"
+
+    participantsDiv =
+      div [ class "thread-participants" ]
+        [ text ("Between " ++ participantString ++ participantStringEnd) ]
+
+    replyButtonDiv =
+      div [ class "btn-bar" ]
+        [ button [ class "btn btn-small"
+                 , onClick showReplyMessage
+                 ]
+            [ text "Reply" ]
+        ]
+
+    replyBoxDiv =
+      case maybeReply of
+        Just reply ->
+          if reply.recipients == thread.participants then
+            div []
+              [ textarea [ rows 4
+                         , value reply.body
+                         , onInput updateReplyMessage
+                         ]
+                  [ text reply.body ]
+              , div [ class "btn-bar" ]
+                  [ button
+                    [ class "btn btn-default btn-small"
+                    , onClick sendReplyMessage
+                    ]
+                    [ text "Send" ]
+                  , button
+                    [ class "btn btn-small"
+                    , onClick closeReplyMessage
+                    ]
+                    [ text "Close" ]
+                  ]
+              ]
+          else
+            replyButtonDiv
+
+        Nothing ->
+          replyButtonDiv
+  in
+    li []
+      (List.concat
+        [ [ participantsDiv ]
+        , List.map messageView thread.messages
+        , [ replyBoxDiv ]
+        ])
 
 
 loadingView : Maybe Banner -> Html msg
 loadingView maybeBanner =
-    div []
-        [ bannerView maybeBanner
-        , text "Loading…"
-        ]
+  div []
+    [ bannerView maybeBanner
+    , text "Loading…"
+    ]
 
 
 bannerView : Maybe Banner -> Html msg
 bannerView maybeBanner =
-    case maybeBanner of
-        Just banner ->
-            div [ class <| "banner banner-" ++ banner.type_ ]
-                [ text banner.text ]
+  case maybeBanner of
+    Just banner ->
+      div [ class <| "banner banner-" ++ banner.type_ ]
+        [ text banner.text ]
 
-        Nothing ->
-            text ""
+    Nothing ->
+      text ""
 
 
 linkTo : (String -> msg) -> String -> List (Html.Attribute msg)
 linkTo message url =
-    [ href url
-    , onPreventDefaultClick (message url)
-    ]
+  [ href url
+  , onPreventDefaultClick (message url)
+  ]
 
 
 breadcrumbView : (String -> msg) -> Breadcrumb -> Html msg
 breadcrumbView messageConstructor link =
-    a (linkTo messageConstructor link.url)
-        [ text link.title ]
+  a (linkTo messageConstructor link.url)
+    [ text link.title ]
 
 
 breadcrumbNavView : (String -> msg) -> List Breadcrumb -> Html msg -> Html msg
 breadcrumbNavView messageConstructor links pageTitle =
-    let
-        parts =
-            List.concat
-                [ (List.map (breadcrumbView messageConstructor) links)
-                , [ pageTitle ]
-                ]
-    in
-        nav [ class "breadcrumbs" ]
-            (List.intersperse
-                (text " ⇢ ")
-                parts
-            )
+  let
+    parts =
+      List.concat
+        [ (List.map (breadcrumbView messageConstructor) links)
+        , [ pageTitle ]
+        ]
+  in
+    nav [ class "breadcrumbs" ]
+      (List.intersperse
+        (text " ⇢ ")
+        parts)
 
 
 unpublishedChapterView : (String -> msg) -> Narration -> ChapterOverview -> Html msg
@@ -266,17 +251,24 @@ narrationOverviewView navigationMessage narrationOverview =
 compactNarrationView : (String -> msg) -> NarrationOverview -> Html msg
 compactNarrationView navigationMessage overview =
   let
-    buttonBar = case overview.narration.status of
-                  Active ->
-                    button [ class "btn btn-add"
-                           , onClick (navigationMessage <| "/narrations/" ++ (toString overview.narration.id) ++ "/new")
-                           ]
-                      [ text "New chapter" ]
-                  _ ->
-                    text ""
+    (ribbon, buttonBar) =
+      case overview.narration.status of
+        Active ->
+          ( text ""
+          , button [ class "btn btn-add"
+                   , onClick (navigationMessage <| "/narrations/" ++ (toString overview.narration.id) ++ "/new")
+                   ]
+              [ text "New chapter" ]
+          )
+        _ ->
+          ( div [ class "corner-ribbon top-right" ]
+              [ text <| narrationStatusString overview.narration.status ]
+          , text ""
+          )
   in
     div [ class "narration-container" ]
-      [ div [ class "narration-header" ]
+      [ ribbon
+      , div [ class "narration-header" ]
           [ h2 []
             [ a
                 (linkTo
